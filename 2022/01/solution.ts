18304619878a881d50b1a893dd50bd70b400387b
Oliver Böhm <oliverboehmprivat@gmail.com>
2022-12-14 11:41:23 +0100

    feat(01-22): Add partOne solution

--- 2022/01/solution.ts
+++ 2022/01/solution.ts
@@ -1,8 +1,33 @@
-function partTwo() {
-  const input: string = fs
+import {fs} from 'fs';
+
+const input: string = fs
     .readFileSync(__dirname + "/input.txt", "utf-8")
     .trim();
 
+const isNewLine = (char) => char.charCodeAt(0) === 10;
+
+function partOne() {
+  let mostColories = 0;
+
+  let calory = "";
+  let elve = 0;
+  for (let i = 0; i < input.length; i++) {
+    calory += input[i];
+    if (isNewLine(input[i])) {
+      elve += Number(calory);
+      calory = "";
+
+      if (isNewLine(input[i + 1])) {
+        mostColories = mostColories > elve ? mostColories : elve;
+        elve = 0;
+      }
+    }
+  }
+
+  return mostColories;
+}
+
+function partTwo() {
   let accumulator: number = 0;
   return elvesCalories
     .trim()
