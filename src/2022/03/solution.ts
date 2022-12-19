c7b3c4dbf2831737d75fbde89efcc2a9dc78c30e
Oliver Boehm <oboehm@medienwerft.de>
2022-12-19 20:56:35 +0100

    Fix small bug

--- src/2022/03/solution.ts
+++ src/2022/03/solution.ts
@@ -1,45 +1,43 @@
 import { getInput } from "../input.js";
 
-const input: Array<string> = getInput()
+const input: Array<string> = getInput();
 
-const getPriority = (char: string): number =>  char.charCodeAt(0) -
-            (char.toLowerCase() === char ? 96 : 38);
+const getPriority = (char: string): number =>
+  char.charCodeAt(0) - (char.toLowerCase() === char ? 96 : 38);
 
 function getItemSum(): number {
-    let total: number = 0;
-  
-    input.forEach((items) => {
-      const half: number = items.length / 2;
-      const left: Set<string> = new Set<string>(items.substring(0, half));
-  
-      for (let i: number = half; i < items.length; i++) {
-        if (left.has(items[i])) {
-          total += getPriority(items[i])
-          break;
-        }
+  let total: number = 0;
+
+  input.forEach((items) => {
+    const half: number = items.length / 2;
+    const left: Set<string> = new Set<string>(items.substring(0, half));
+
+    for (let i: number = half; i < items.length; i++) {
+      if (left.has(items[i])) {
+        total += getPriority(items[i]);
+        break;
       }
-    });
-  
-    return total;
-  }
-  
-  function getBadgeTotal(): number {
-    let total = 0
-
-    const items = input.map(rucksack => rucksack.trim())
-
-    for(let i = 0; i < items.length; i += 3) {
-        const first = new Set(items[i])
-        const second = new Set(items[i + 1])
-
-        for(let j = 0; j < items[2].length; j++) {
-            const char: string = items[i + 2][j] 
-            if (first.has(char) && second.has(char)) {
-                total += getPriority(char) 
-                break
-            }
-        }
     }
+  });
 
-    return total
+  return total;
+}
+
+function getBadgeTotal(): number {
+  let total = 0;
+
+  for (let i = 0; i < input.length; i += 3) {
+    const first = new Set(input[i]);
+    const second = new Set(input[i + 1]);
+
+    for (let j = 0; j < input[i + 2].length; j++) {
+      const char: string = input[i + 2][j];
+      if (first.has(char) && second.has(char)) {
+        total += getPriority(char);
+        break;
+      }
+    }
+  }
+
+  return total;
 }
