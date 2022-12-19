faf21f8ceb313adc71e65790ff894afe7ad6868d
Oliver Boehm <oboehm@medienwerft.de>
2022-12-19 18:35:18 +0100

    Refactore

--- src/2022/01/solution.ts
+++ src/2022/01/solution.ts
@@ -1,30 +1,20 @@
-import {fs} from 'fs';
+import { getInput } from "../input.js";
 
-const input: string = fs
-    .readFileSync(__dirname + "/input.txt", "utf-8")
-    .trim();
+const input: Array<number> = getInput().map(Number);
 
-const isNewLine = (char) => char.charCodeAt(0) === 10;
+export function partOne() {
+  let most = 0;
 
-function partOne() {
-  let mostColories = 0;
-
-  let calory = "";
   let elve = 0;
-  for (let i = 0; i < input.length; i++) {
-    calory += input[i];
-    if (isNewLine(input[i])) {
-      elve += Number(calory);
-      calory = "";
-
-      if (isNewLine(input[i + 1])) {
-        mostColories = mostColories > elve ? mostColories : elve;
-        elve = 0;
-      }
+  input.forEach((calory: number) => {
+    elve += calory;
+    if (calory === 0) {
+      most = elve > most ? elve : calory;
+      elve = 0;
     }
-  }
+  });
 
-  return mostColories;
+  return most;
 }
 
 function partTwo() {
@@ -32,19 +22,16 @@ function partTwo() {
   let top = [lowest, lowest, lowest];
 
   let elve = 0;
-  input
-    .split("\n")
-    .map(Number)
-    .forEach((calory) => {
-      elve += calory;
-      if (calory === 0) {
-        if (elve > top[0]) {
-          top[0] = elve;
-          top.sort((a, b) => a - b);
-        }
-        elve = 0;
+  input.forEach((calory) => {
+    elve += calory;
+    if (calory === 0) {
+      if (elve > top[0]) {
+        top[0] = elve;
+        top.sort((a, b) => a - b);
       }
-    });
+      elve = 0;
+    }
+  });
 
   return top[0] + top[1] + top[2];
 }
